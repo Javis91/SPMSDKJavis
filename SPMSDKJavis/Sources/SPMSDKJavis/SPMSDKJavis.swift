e81e6c86f856195d88c0663c04a46442405c21af
Javier Picazo Hernández <francisco.picazo@ordenaris.com>
2024-04-24 16:45:20 -0600

    Create SDK Project by Swift Package Manager

---
+++ SPMSDKJavis/Sources/SPMSDKJavis/SPMSDKJavis.swift
@@ -0,0 +1,16 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+public class SPM{
+    private var message: String
+    
+    public init(message: String) {
+        self.message = message
+    }
+    
+    
+    public func printMessage(){
+        print(self.message)
+    }
+}
---
+++ SPMSDKJavis/Tests/SPMSDKJavisTests/SPMSDKJavisTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import SPMSDKJavis
+
+final class SPMSDKJavisTests: XCTestCase {
+    func testExample() throws {
+        let user = SPM(message: "utito")
+        user.printMessage()
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
